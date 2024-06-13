4736eb7009767a574f6c1fb377c3f83067efd621
Barshon <barshonweb@gmail.com>
2024-06-13 21:31:10 +0600

    dashboard and sidebar design

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Drawer from "../Sidebar-Drawer/Drawer";
+import Sidebar from "../Sidebar-Drawer/Sidebar";
 
 const Dashboard = () => {
     return (
-        <div>
-            drawer
+        <div className="grid md:grid-flow-col">
+           <div className="bg-orange-700 text-white min-h-screen col-span-2 hidden md:block">
+           <Sidebar></Sidebar>
+           </div>
+           <div className="md:hidden">
+           <Drawer></Drawer>
+           </div>
+            <div className="md:col-span-10">
             <Outlet></Outlet>
+            </div>
         </div>
     );
 };
---
+++ src/Sidebar-Drawer/Drawer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Drawer = () => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <label htmlFor="my-drawer" className="btn flex justify-center mx-20 mt-2 bg-orange-700 text-white drawer-button">
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-72 min-h-full bg-orange-700 text-white">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
