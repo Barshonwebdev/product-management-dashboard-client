2353661d7616f80ffd6b15440d67e648ff41544f
Barshon <barshonweb@gmail.com>
2024-06-13 10:40:41 +0600

    authprovider for neccesary login/signup functions

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
+export const AuthContext=createContext(null);
+const auth=getAuth(app);
+const googleProvider=new GoogleAuthProvider();
+const AuthProvider = ({children}) => { 
+    const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true);
+    
+    useEffect(()=>{
+        const unsubscribe= onAuthStateChanged(auth,currentuser=>{
+            setUser(currentuser);
+            console.log(currentuser);
+            setLoading(false);
+        })
 
-const AuthProvider = ({children}) => {
+        return ()=>{
+            return unsubscribe();
+        }
+    },[]);
+
+    const createUser=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+   
+    const signinUser=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const logout=()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const googleSignin=()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider);
+    }
+    
+    const authInfo={user,loading,createUser,signinUser,logout,googleSignin};
     return (
-        <div>
-            
-        </div>
+      <div>
+        <AuthContext.Provider value={authInfo}>
+          {children}
+        </AuthContext.Provider>
+      </div>
     );
 };
 
